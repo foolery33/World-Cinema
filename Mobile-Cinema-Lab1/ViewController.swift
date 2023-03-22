2d4d0bca0c52cbc56540936d5f7762bdc69f2c62
Nikita Usov <usov107@gmail.com>
2023-03-22 13:26:03 +0700

    Initial Commit

---
+++ Mobile-Cinema-Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile-Cinema-Lab1
+//
+//  Created by admin on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
