aab553c1feea9b83f47bbb7f78489149c586b3a0
Usov Nikita <usov107@gmail.com>
2023-04-20 23:14:42 +0700

    Добавил недобавленный к предыдущему коммиту файл SceneDelegate

---
+++ Mobile-Cinema-Lab1/SceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  Mobile-Cinema-Lab1
+//
+//  Created by admin on 20.04.2023.
+//
+
+import UIKit
+import RealmSwift
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let collectionsDatabase = CollectionsDatabase(realm: setupRealm())
+        
+        let navigationController = UINavigationController()
+        appCoordinator = AppCoordinator(navigationController: navigationController, collectionsDatabase: collectionsDatabase)
+        appCoordinator?.start()
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    func setupRealm() -> Realm {
+        do {
+            return try Realm()
+        } catch let error {
+            fatalError("Failed to create Realm: \(error.localizedDescription)")
+        }
+    }
+
+}
+
